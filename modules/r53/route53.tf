bc4eabcba8181548cdd969ccdcacaa06fe874194
Deepak Patil <deepakshivajipatil@rccl.com>
2024-02-21 12:36:55 +0530

    Deleted extra folder

--- modules/r53/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "private" {
-  name               = "example.com"
-  vpc {
-    vpc_id = aws_vpc.main.id
-    # Add more VPC configurations as needed
-  }
-  comment            = "Private hosted zone for internal DNS resolution"
-}
